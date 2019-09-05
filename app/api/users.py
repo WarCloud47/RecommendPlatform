978fae5319ec919aa1dc5635b75748c67b909559
WarCloud47 <zenlunatic47@gmail.com>
2019-09-05 10:46:38 +0400

    test commit

--- app/api/users.py
+++ app/api/users.py
@@ -14,6 +14,7 @@ def get_user(id):
 def get_users():
     page = request.args.get('page', 1, type=int)
     per_page = min(request.args.get('per_page', 10, type=int), 100)
+    print("test")
     data = User.to_collection_dict(User.query, page, per_page, 'api.get_users')
     return jsonify(data)
 
